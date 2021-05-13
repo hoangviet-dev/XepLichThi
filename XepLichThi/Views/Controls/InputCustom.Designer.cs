5b304fa1115fa71695baaf4144196dd5428880ba
Hoàng Việt Nguyễn <viet070420@gmail.com>
2021-05-13 08:26:21 +0700

    first commit

---
+++ XepLichThi/Views/Controls/InputCustom.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace XepLichThi.Views.Controls
+{
+    partial class InputCustom
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtInput.Location = new System.Drawing.Point(0, 0);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(255, 47);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "abc";
+            this.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // InputCustom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtInput);
+            this.Name = "InputCustom";
+            this.Size = new System.Drawing.Size(255, 47);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
