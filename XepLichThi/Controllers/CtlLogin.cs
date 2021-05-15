a46bd38ba0cd5682232492a4d63c17572994fc27
Khoa Lê <khoavida@gmail.com>
2021-05-16 00:18:09 +0700

    Su dung proc, loi chua fix

--- XepLichThi/Controllers/CtlLogin.cs
+++ XepLichThi/Controllers/CtlLogin.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using XepLichThi.Models;
+using static XepLichThi.Models.DataProvider;
 
 namespace XepLichThi.Controllers
 {
@@ -18,10 +20,42 @@ namespace XepLichThi.Controllers
 
         public bool CheckLogin(string username, string password)
         {
-            string sql = "select * from TaiKhoan where UserName = @UserName and Password = @Password";
-            object[] param = { username, password};
-            DataTable data = provider.excuteQuery(sql, param);
-            return data.Rows.Count>0;
+            //string sql = "select * from TaiKhoan where UserName = @UserName and Password = @Password";
+            //object[] param = { username, password};
+            //DataTable data = provider.excuteQuery(sql, param);
+            //return data.Rows.Count>0
+            //---------------
+            //int res = 0;
+            //string sql = "proc_TK_Dang_Nhap";
+
+            //SqlParameter[] ParamIn = new SqlParameter[2];
+            //SqlParameter[] ParamOut = new SqlParameter[1];
+
+            //ParamIn[0] = new SqlParameter("@UserName", username);
+            //ParamIn[1] = new SqlParameter("@Password", password);
+
+            //ParamOut[0] = new SqlParameter("@Type", res);
+
+            //object[] khoa = provider.excuteProc(sql, ParamIn, ParamOut);
+            //Console.WriteLine(khoa);
+            //return false;
+            //int res = provider.executeLogin(username, password);
+            //Console.WriteLine(res);
+
+            string sql = "EXEC proc_TK_Dang_Nhap @UserName,@Password,@Type OUT";
+
+            SqlParam[] paramIn =
+            {
+                new SqlParam("@UserName", username),
+                new SqlParam("@Password", password)
+            };
+
+            SqlParam[] paramOut = { new SqlParam("@Type", 1) };
+
+            object[] data = provider.excuteProc(sql, paramIn, paramOut);
+
+            Console.WriteLine(data);
+            return false;
         }
 
     }
--- XepLichThi/Models/DataProvider.cs
+++ XepLichThi/Models/DataProvider.cs
@@ -11,7 +11,7 @@ namespace XepLichThi.Models
 {
     class DataProvider
     {
-        private string connectionString = @"Data Source=BAOLAPTOP;Initial Catalog=XepLichThi;Integrated Security=True";
+        private string connectionString = @"Data Source=LAPTOP\SQLEXPRESS;Initial Catalog=XepLichThi;Integrated Security=True";
 
         public DataProvider()
         {
@@ -76,32 +76,70 @@ namespace XepLichThi.Models
             return data;
         }
 
-        public object excuteProc(string procName, List<SqlParam> parameter = null)
+        public object[] excuteProc(string procName, SqlParam[] parameterIn = null, SqlParam[] parameterOut = null)
         {
-            object data = null;
+            object[] data = null;
             using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(procName, connection))
             {
-                connection.Open();
-                SqlCommand cmd = new SqlCommand(procName, connection);
-                cmd.CommandType = CommandType.StoredProcedure;
-                if (parameter != null)
+                cmd.CommandType = CommandType.Text;
+
+                if (parameterIn != null)
                 {
-                    foreach (SqlParam param in parameter)
+                    for (int i=0; i<parameterIn.Length; i++)
                     {
-                        cmd.Parameters.AddWithValue("@" + param.Name, param.Value);
+                        cmd.Parameters.AddWithValue(parameterIn[i].Name, parameterIn[i].Value);
                     }
                 }
-                data = cmd.ExecuteReader();
+                if (parameterOut != null)
+                {
+                    for (int i = 0; i < parameterOut.Length; i++)
+                    {
+                        cmd.Parameters.AddWithValue(parameterOut[i].Name, parameterOut[i].Value).Direction = ParameterDirection.Output;
+                    }
+                }
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+                for (int i = 0; i < parameterOut.Length; i++)
+                {
+                    data[i] = (object)cmd.Parameters[parameterOut[i].Name].Value;
+                }
+
                 connection.Close();
             }
             return data;
         }
 
+        public int executeLogin(string username, string password)
+        {
+            int res = 0;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand("EXEC proc_TK_Dang_Nhap @UserName,@Password,@Type OUT", conn))
+            {
+                cmd.CommandType = CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@UserName", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+                cmd.Parameters.AddWithValue("@Type", 1).Direction = ParameterDirection.Output;
+
+                Console.WriteLine(cmd.Parameters);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+                res = Convert.ToInt32(cmd.Parameters["@Type"].Value);
+                conn.Close();
+            }
+            return res;
+        }
+
         public class SqlParam
         {
             public string Name;
-            public string Value;
-            public SqlParam(string name, string value)
+            public object Value;
+            public SqlParam(string name, object value)
             {
                 Name = name;
                 Value = value;
