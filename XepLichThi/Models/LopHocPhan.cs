eb3c3d58d2a70968b73192a0b11cda8697a68626
baro-dev <62060371+baro-dev@users.noreply.github.com>
2021-05-16 02:37:57 +0700

    Add displayName

--- XepLichThi/Models/LopHocPhan.cs
+++ XepLichThi/Models/LopHocPhan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,8 +16,13 @@ namespace XepLichThi.Models
             SoTinChi = soTinChi;
         }
 
+        [DisplayName("Mã lớp học phần")]
         public string MaLopHocPhan { get; set; }
+
+        [DisplayName("Tên lớp học phần")]
         public string TenLopHocPhan { get; set; }
+
+        [DisplayName("Số tín chỉ")]
         public int SoTinChi { get; set; }
 
     }
